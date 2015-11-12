f3a79520c7ed1e6fc65370c485ef02c045a14b51
KevinSanz <kevinbm2007@hotmail.com>
2015-11-12 18:30:42 +0100

    Terminado ejercicio Base 4
    he colocado el rectángulo en la mitad

---
+++ src/Repaso4.java
@@ -0,0 +1,42 @@
+/*
+ * Repaso3: Pinta un rect‡ngulo en la esquina superior izquierda
+ * 
+ */
+//importamos la librer’a acm para los objetos gr‡ficos
+import acm.graphics.*;
+
+public class Repaso4 extends acm.program.GraphicsProgram{
+
+	//declaro una variable de instancia para guardar el rect‡ngulo
+	//como es de intancia se desde el init y desde el run
+	GRect rectangulo;
+
+	//declaro una variable de instancia que guardara el punto X de las 
+	//pantalla en que empezara a pintarse el rectangulo
+	int distanciaX;
+	int distanciaY;
+
+
+	//recuerda que el init se ejecuta siempre antes que el run
+	public void init(){
+		//tama–o de la pantalla 
+		setSize(800,600);
+		//el primer par‡metro del rect‡ngulo es el ancho
+		//y el segundo el alto
+		rectangulo = new GRect(120,80);
+		add(rectangulo);
+
+	}
+
+	public void run(){
+		//divido el ancho de la pantalla entre dos para saber donde 
+		//esta la mitad exancta de la pantalla
+		distanciaX = getWidth()/2;
+		distanciaY = getHeight()/2;
+
+		//a–ado el objeto GRect al lienzo para que se muestre
+		//si no especifico la posici—n, aparece en (0,0)
+		rectangulo.setLocation(distanciaX-60, distanciaY-40);
+	}
+
+}
