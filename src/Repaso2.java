8d0f04b8147b497f6ff7921626b4e7e6f04d5d73
KevinSanz <kevinbm2007@hotmail.com>
2015-11-12 18:08:08 +0100

    triángulo en la mitad

--- src/Repaso2.java
+++ src/Repaso2.java
@@ -8,21 +8,33 @@ import acm.graphics.*;
 public class Repaso2 extends acm.program.GraphicsProgram{
 
 	//declaro una variable de instancia para guardar el rect‡ngulo
+	//como es de intancia se desde el init y desde el run
 	GRect rectangulo;
+	
+	//declaro una variable de instancia que guardara el punto X de las 
+	//pantalla en que empezara a pintarse el rectangulo
+	int distanciaX;
 	
 	
 	//recuerda que el init se ejecuta siempre antes que el run
 	public void init(){
+		//tama–o de la pantalla 
 		setSize(800,600);
 		//el primer par‡metro del rect‡ngulo es el ancho
 		//y el segundo el alto
 		rectangulo = new GRect(120,80);
+		add(rectangulo);
 		
 	}
 	
 	public void run(){
+		//divido el ancho de la pantalla entre dos para saber donde 
+		//esta la mitad exancta de la pantalla
+		distanciaX = getWidth()/2;
+		
 		//a–ado el objeto GRect al lienzo para que se muestre
 		//si no especifico la posici—n, aparece en (0,0)
-		add(rectangulo,getWidth()/2-60,0);
+		rectangulo.setLocation(distanciaX-60, 0);
 	}
+	
 }
